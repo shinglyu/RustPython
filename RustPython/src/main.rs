e782145a5a3487814321653ae81a613ba537e0df
Shing Lyu <shing.lyu@gmail.com>
2016-10-25 18:56:08 +0800

    Parse bytecode from file. Fixed the bytecode compiler output to use index instead of the actual value

--- RustPython/src/main.rs
+++ RustPython/src/main.rs
@@ -2,6 +2,7 @@
 //use eval::eval::*;
 use std::io::prelude::*;
 use std::fs::File;
+use std::env;
 
 struct VirtualMachine {
     consts: Vec<Option<i32>>, // Dynamic typing?
@@ -56,28 +57,81 @@ impl VirtualMachine {
     }
 }
 
+#[derive(PartialEq, Debug)]
+struct Code<'a> {
+    consts: Vec<Option<i32>>,
+    op_codes: Vec<(&'a str, Option<i32>)>
+}
+
+fn parse_bytecode(s: &str) -> Code {
+    let lines: Vec<&str> = s.split('\n').collect();
+
+    let (metadata, ops) = lines.split_at(2);
+
+    // Parsing the first line CONSTS
+    let consts_str: &str = metadata[0]; // line 0 is empty
+    let values_str = &consts_str[("CONSTS: (".len())..(consts_str.len()-1)];
+    let values: Vec<&str> = values_str.split(", ").collect();
+    // We need better type definition here
+    let consts: Vec<Option<i32>>= values.into_iter().map(|x| x.parse::<i32>().ok()).collect();
+
+    // Parsing the op_codes
+    let op_codes: Vec<(&str, Option<i32>)>= ops.into_iter()
+                                               .map(|x| x.trim())
+                                               .filter(|x| x.len() > 0)
+                                               .map(|x| {
+                                                   let op: Vec<&str> = x.split(", ").collect();
+                                                   println!("{:?}", op);
+                                                   (op[0], op[1].parse::<i32>().ok())
+                                               }).collect();
+    
+
+    Code {
+        consts: consts,
+        op_codes: op_codes,
+    }
+
+}
+
 fn main() {
     // TODO: read this from args
-    let filename = "../test.bytecode";
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
 
     let mut f = File::open(filename).unwrap();
     let mut s = String::new();
     f.read_to_string(&mut s).unwrap();
-    println!("{}", s);
+    let code = parse_bytecode(&s);
 
-    let input = vec![
-        ("LOAD_CONST", Some(2)),
-        ("PRINT_ITEM", None),
-        ("PRINT_NEWLINE", None),
-        ("LOAD_CONST", None),
-        ("RETURN_VALUE", None)
-    ];
+    let mut vm = VirtualMachine::new(code.consts);
+    vm.exec(code.op_codes);
+}
 
-    // TODO: read this from file
-    let consts = vec![Some(1), None, Some(2)];
+#[test]
+fn test_parse_bytecode() {
 
-    let mut vm = VirtualMachine::new(consts);
-    vm.exec(input);
+    let input = "CONSTS: (1, None, 2)
+VARNAMES: ()
+SetLineno, 1
+LOAD_CONST, 2
+PRINT_ITEM, None
+PRINT_NEWLINE, None
+LOAD_CONST, None
+RETURN_VALUE, None
+        ";
+    let expected = Code { // Fill me with a more sensible data
+        consts: vec![Some(1), None, Some(2)], 
+        op_codes: vec![
+            ("SetLineno".to_string(), Some(1)),
+            ("LOAD_CONST".to_string(), Some(2)),
+            ("PRINT_ITEM".to_string(), None),
+            ("PRINT_NEWLINE".to_string(), None),
+            ("LOAD_CONST".to_string(), None),
+            ("RETURN_VALUE".to_string(), None)
+        ]
+    };
+
+    assert_eq!(expected, parse_bytecode(input));
 }
 
 #[test]
--- compile_code.py
+++ compile_code.py
@@ -20,4 +20,20 @@ c = byteplay.Code.from_code(code)
 # print(list(c.code))
 
 for op in c.code:
-    print("{0}, {1}".format(op[0], op[1]))
+    # Byteplay print the actual argument instead of the index. But we are
+    # Converting them back to the index in case we want to use other bytecode
+    # disassembler in the future.
+    # Here is a list of meanings for the arguments
+    #   The argument of opcodes in hasconst is the actual constant.
+    #   The argument of opcodes in hasname is the name, as a string.
+    #   The argument of opcodes in hasjump is a Label instance, which should point to a specific location in the code list.
+    #   The argument of opcodes in haslocal is the local variable name, as a string.
+    #   The argument of opcodes in hascompare is the string representing the comparison operator.
+    #   The argument of opcodes in hasfree is the name of the cell or free variable, as a string.
+    if op[0] in byteplay.hasconst:
+        print("{0}, {1}".format(op[0], code.co_consts.index(op[1])))
+    else:
+        print("{0}, {1}".format(op[0], op[1]))
+
+
+
