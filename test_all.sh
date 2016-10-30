3479a5f5e32b09b91be0732ad893083394c29faf
Shing Lyu <shing.lyu@gmail.com>
2016-10-30 22:22:54 +0800

    Added integration test script

---
+++ test_all.sh
@@ -0,0 +1,13 @@
+#TESTCASE=tests/variables.py
+# TESTCASE=tests/variables.py
+#TESTCASE=tests/minimum.py
+for TESTCASE in $(find tests -name \*.py -print)
+do
+  echo "TEST START: ${TESTCASE}"
+  echo "--------------------------------"
+  python compile_code.py $TESTCASE > $TESTCASE.bytecode
+  cd RustPython
+  cargo run ../$TESTCASE.bytecode
+  cd ..
+  echo "--------------------------------"
+done
