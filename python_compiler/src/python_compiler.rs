e78dfe6d30dc1c533d63fcee65349045387affa4
Shing Lyu <shing.lyu@gmail.com>
2016-11-07 20:49:21 +0800

    Try to call compiler

--- python_compiler/src/python_compiler.rs
+++ python_compiler/src/python_compiler.rs
@@ -1,19 +1,23 @@
-extern crate cpython;
+use cpython::Python;
+use cpython::ObjectProtocol; //for call method
+use cpython::PyObject;
+use python27_sys::PyCodeObject;
 
-use self::cpython::Python;
-use self::cpython::ObjectProtocol; //for call method
 
-pub fn compile() -> String{
+pub fn compile() -> PyObject {
     let gil = Python::acquire_gil();
     let py = gil.python();
 
+    /*
     let sys = py.import("sys").unwrap();
     let version: String = sys.get(py, "version").unwrap().extract(py).unwrap();
 
     let os = py.import("os").unwrap();
     let getenv = os.get(py, "getenv").unwrap();
     let user: String = getenv.call(py, ("USER",), None).unwrap().extract(py).unwrap();
-
-    version
-
+    */
+    let builtin = py.import("__builtin__").unwrap();
+    let compile = builtin.get(py, "compile").unwrap();
+    let code: PyObject = compile.call(py, ("1+1", "foo", "exec"), None).unwrap();
+    code
 }
