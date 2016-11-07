362730aabe3f74e3cc8f60529f90db268934fda1
Shing Lyu <shing.lyu@gmail.com>
2016-11-08 00:14:37 +0800

    Compile directly to PyCodeObject in Rust

--- compile_code.py
+++ compile_code.py
@@ -3,49 +3,58 @@ import byteplay
 import sys
 # from pprint import pprint
 
-filename = sys.argv[1]
-with open(filename, 'rU') as f:
-    code = f.read()
 
-code = compile(code, filename, "exec")
-print("CONSTS: {0}".format(code.co_consts))
-print("NAMES: {0}".format(code.co_names))
+def parse_co_code_to_str(code):
+    c = byteplay.Code.from_code(code)
+# pprint(c.code)
+# print(list(c.code))
+
+    def format_label(l):
+        return "LABEL, {0}".format(id(l))
+
+    for op in c.code:
+        # Byteplay print the actual argument instead of the index. But we are
+        # Converting them back to the index in case we want to use other bytecode
+        # disassembler in the future.
+        # Here is a list of meanings for the arguments
+        #   The argument of opcodes in hasconst is the actual constant.
+        #   The argument of opcodes in hasname is the name, as a string.
+        #   The argument of opcodes in hasjump is a Label instance, which should point to a specific location in the code list.
+        #   The argument of opcodes in haslocal is the local variable name, as a string.
+        #   The argument of opcodes in hascompare is the string representing the comparison operator.
+        #   The argument of opcodes in hasfree is the name of the cell or free variable, as a string.
+        if op[0] in byteplay.hasconst:
+            print("{0}, {1}".format(op[0], code.co_consts.index(op[1])))
+            #print("{0}, {1}".format(op[0], None))
+        elif op[0] in byteplay.hasname:
+            print("{0}, {1}".format(op[0], code.co_names.index(op[1])))
+        elif op[0] in byteplay.hasjump:
+            print("{0}, {1}".format(op[0], id(op[1])))
+        elif op[0] in byteplay.hascompare:
+            print("{0}, {1}".format(op[0], byteplay.cmp_op.index(op[1])))
+        elif type(op[0]) == byteplay.Label:
+            assert(op[1] == None)
+            print(format_label(op[0])) # Is the second argument always None?
+            # print("{0}, {1}".format(format_label(op[0]), op[1]))
+        else:
+            print("{0}, {1}".format(op[0], op[1]))
+
+def main():
+
+    filename = sys.argv[1]
+    with open(filename, 'rU') as f:
+        code = f.read()
+
+    code = compile(code, filename, "exec")
+    print("CONSTS: {0}".format(code.co_consts))
+    print("NAMES: {0}".format(code.co_names))
 # print(code.co_varnames)
 # print(list(bytearray(code.co_code)))
 # for elem in list(bytearray(code.co_code)):
 #     print(dis.opname[elem])
 # dis_output = dis.dis(code)
 
-c = byteplay.Code.from_code(code)
-# pprint(c.code)
-# print(list(c.code))
+    parse_co_code_to_str(code)
 
-def format_label(l):
-    return "LABEL, {0}".format(id(l))
-
-for op in c.code:
-    # Byteplay print the actual argument instead of the index. But we are
-    # Converting them back to the index in case we want to use other bytecode
-    # disassembler in the future.
-    # Here is a list of meanings for the arguments
-    #   The argument of opcodes in hasconst is the actual constant.
-    #   The argument of opcodes in hasname is the name, as a string.
-    #   The argument of opcodes in hasjump is a Label instance, which should point to a specific location in the code list.
-    #   The argument of opcodes in haslocal is the local variable name, as a string.
-    #   The argument of opcodes in hascompare is the string representing the comparison operator.
-    #   The argument of opcodes in hasfree is the name of the cell or free variable, as a string.
-    if op[0] in byteplay.hasconst:
-        print("{0}, {1}".format(op[0], code.co_consts.index(op[1])))
-        #print("{0}, {1}".format(op[0], None))
-    elif op[0] in byteplay.hasname:
-        print("{0}, {1}".format(op[0], code.co_names.index(op[1])))
-    elif op[0] in byteplay.hasjump:
-        print("{0}, {1}".format(op[0], id(op[1])))
-    elif op[0] in byteplay.hascompare:
-        print("{0}, {1}".format(op[0], byteplay.cmp_op.index(op[1])))
-    elif type(op[0]) == byteplay.Label:
-        assert(op[1] == None)
-        print(format_label(op[0])) # Is the second argument always None?
-        # print("{0}, {1}".format(format_label(op[0]), op[1]))
-    else:
-        print("{0}, {1}".format(op[0], op[1]))
+if __name__ == "__main__":
+    main()
--- python_compiler/src/python_compiler.rs
+++ python_compiler/src/python_compiler.rs
@@ -1,23 +1,34 @@
 use cpython::Python;
 use cpython::ObjectProtocol; //for call method
 use cpython::PyObject;
+use cpython::PyDict;
 use python27_sys::PyCodeObject;
 
 
-pub fn compile() -> PyObject {
+//pub fn compile() -> PyObject {
+pub fn compile(){
     let gil = Python::acquire_gil();
     let py = gil.python();
 
-    /*
-    let sys = py.import("sys").unwrap();
-    let version: String = sys.get(py, "version").unwrap().extract(py).unwrap();
+    let locals = PyDict::new(py);
+    // TODO: read the filename from commandline
+    locals.set_item(py, "filename", "../tests/functions.py");
 
-    let os = py.import("os").unwrap();
-    let getenv = os.get(py, "getenv").unwrap();
-    let user: String = getenv.call(py, ("USER",), None).unwrap().extract(py).unwrap();
-    */
-    let builtin = py.import("__builtin__").unwrap();
-    let compile = builtin.get(py, "compile").unwrap();
-    let code: PyObject = compile.call(py, ("1+1", "foo", "exec"), None).unwrap();
-    code
+    let load_file = "\
+import os
+print(os.getcwd())
+with open(filename, 'rU') as f:\
+    code = f.read()\
+";
+    py.run(load_file, None, Some(&locals)).unwrap();
+    let code = py.eval("compile(code, \"foo\", \"exec\")", None, Some(&locals)).unwrap();
+    println!("{:?}", code.getattr(py, "co_name").unwrap());
+    println!("{:?}", code.getattr(py, "co_filename").unwrap());
+    println!("{:?}", code.getattr(py, "co_code").unwrap());
+    println!("{:?}", code.getattr(py, "co_freevars").unwrap());
+    println!("{:?}", code.getattr(py, "co_cellvars").unwrap());
+    println!("{:?}", code.getattr(py, "co_consts").unwrap());
+    let consts =  code.getattr(py, "co_consts").unwrap();
+    println!("{:?}", consts.get_item(py, 0).unwrap().getattr(py, "co_code"));
+
 }
