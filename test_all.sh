dd4de503d9f0925da0aea27fb549afa2b206ff2e
Shing Lyu <shing.lyu@gmail.com>
2016-11-11 10:45:24 +0800

    Test runner summary

--- test_all.sh
+++ test_all.sh
@@ -1,7 +1,11 @@
+#!/usr/bin/env bash
 # set -e 
 #TESTCASE=tests/variables.py
 # TESTCASE=tests/variables.py
 #TESTCASE=tests/minimum.py
+fails=0
+oks=0
+fail_titles=$""
 for TESTCASE in $(find tests -name \*.py -print)
 do
   echo "TEST START: ${TESTCASE}"
@@ -9,6 +13,23 @@ do
   python compile_code.py $TESTCASE > $TESTCASE.bytecode
   cd RustPython
   cargo run ../$TESTCASE.bytecode
+  if [ $? -ne 0 ]; then
+    echo "== FAIL =="
+    let fails=fails+1
+    fail_titles=$"${fail_titles}\n${TESTCASE}"
+  else
+    echo "==  OK  =="
+    let oks=oks+1
+  fi
   cd ..
   echo "--------------------------------"
+
 done
+
+echo "Summary"
+echo "================"
+echo "${fails} fails, ${oks} passes"
+echo ""
+echo "Fails:"
+echo "$(printf ${fail_titles})"
+echo "================"
